d45485faf52f1915985e20df267178a61d6373b0
Aleks Rutins <keeper@farthergate.com>
2025-11-18 10:42:50 -0500

    Add caching

--- src/server/translate.ts
+++ src/server/translate.ts
@@ -1,4 +1,5 @@
-import { pipeline } from "@huggingface/transformers";
+import { pipeline, type TranslationOutput } from "@huggingface/transformers";
+import { env, redis } from "bun";
 
 const model = await pipeline(
   "translation",
@@ -10,5 +11,13 @@ export default async function translate(
   to: string,
   text: string,
 ) {
-  return await model(text, { src_lang: from, tgt_lang: to } as any);
+  const cache_key = `translated:${from}:${to}:${text}`;
+  if (env.NODE_ENV != "DEVELOPMENT") {
+    const cached = await redis.get(cache_key);
+    if (cached)
+      return JSON.parse(cached) as TranslationOutput | TranslationOutput[];
+  }
+  const out = await model(text, { src_lang: from, tgt_lang: to } as any);
+  if (env.NODE_ENV != "DEVELOPMENT") redis.set(cache_key, JSON.stringify(out));
+  return out;
 }
